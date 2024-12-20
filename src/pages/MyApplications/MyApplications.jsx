d973c68b4b80ef102cedf2b6200a2c03e14e65cc
sadman249 <mdsadman249@gmail.com>
2024-12-20 20:18:42 +0600

    added the server site

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const MyApplications = () => {
     const { user } = useAuth();
     const [jobs, setJobs] = useState([]);
 
+    const axiosSecure = useAxiosSecure();
+
     useEffect(() => {
-        fetch(`https://job-portal-server-five.vercel.app/job-application?email=${user.email}`)
-            .then(res => res.json())
-            .then(data => setJobs(data))
+
+
+        // fetch(`https://job-portal-server-five.vercel.app/job-application?email=${user.email}`)
+        //     .then(res => res.json())
+        //     .then(data => setJobs(data))
+
+        // axios.get(`https://job-portal-server-five.vercel.app/job-application?email=${user.email}`, {
+        //     withCredentials: true
+        // })
+        //     .then(res => setJobs(res.data))
+
+        axiosSecure.get(`/job-application?email=${user.email}`)
+            .then(res => setJobs(res.data));
+
+
     }, [user.email])
 
     return (
